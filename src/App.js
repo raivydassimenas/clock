eee6b927f1f24a405705ad5daee78f23becd248d
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-02-16 11:57:45 +0200

    Implemented countdown function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect, useCallback} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {solid} from '@fortawesome/fontawesome-svg-core/import.macro'
 
@@ -7,31 +7,83 @@ function App() {
 
     const [breakLength, setBreakLength] = useState(5);
     const [sessionLength, setSessionLength] = useState(25);
-    const [timeLeft, setTimeLeft] = useState(25);
+    const [timeLeft, setTimeLeft] = useState("25:00");
     const [startStop, setStartStop] = useState(false);
+
+    const resetClock = () => {
+        setStartStop(false);
+        setBreakLength(5);
+        setSessionLength(25);
+        setTimeLeft("25:00");
+    }
+
+    const breakDecrement = () => {
+        setBreakLength(breakLength => {
+            return breakLength > 1 ? breakLength - 1 : 0;
+        });
+    }
+
+    const breakIncrement = () => {
+        setBreakLength(breakLength => {
+            return breakLength < 60 ? breakLength + 1 : 60;
+        });
+    }
+
+    const sessionDecrement = () => {
+        setSessionLength(sessionLength => {
+            return sessionLength > 1 ? sessionLength - 1 : 0;
+        });
+    }
+
+    const sessionIncrement = () => {
+        setSessionLength(sessionLength => {
+            return sessionLength < 60 ? sessionLength + 1 : 60;
+        });
+    }
+
+    const handleStartStop = () => {
+        setStartStop(!startStop);
+    }
+
+    const countdown = useCallback(() => {
+        let time = timeLeft.split(":");
+        if (time[1] > 0) {
+            time[1]--;
+        } else if (time[0] > 0) {
+            time[0]--;
+            time[1] = 59;
+        }
+        let timeString = [('00' + time[0]).slice(-2), ('00' + time[1]).slice(-2)];
+        setTimeLeft(timeString.join(":"));
+    }, [timeLeft]);
+
+    useEffect(() => {
+        if (startStop) {
+            setTimeout(countdown,1000);
+        }
+    }, [startStop, countdown]);
 
     return (
         <div className="App">
             <div id="break-label">Break Length</div>
             <div id="session-label">Session Length</div>
             <div className="break">
-                <button id="break-decrement"><FontAwesomeIcon icon={solid('circle-minus')}/></button>
+                <button id="break-decrement" onClick={breakDecrement}><FontAwesomeIcon icon={solid('circle-minus')}/></button>
                 <span id="break-length">{breakLength}</span>
-                <button id="break-increment"><FontAwesomeIcon icon={solid('circle-plus')}/></button>
+                <button id="break-increment" onClick={breakIncrement}><FontAwesomeIcon icon={solid('circle-plus')}/></button>
             </div>
             <div className="session">
-                <button id="session-decrement"><FontAwesomeIcon icon={solid("circle-minus")}/></button>
+                <button id="session-decrement" onClick={sessionDecrement}><FontAwesomeIcon icon={solid("circle-minus")}/></button>
                 <span id="session-length">{sessionLength}</span>
-                <button id="session-increment"><FontAwesomeIcon icon={solid('circle-plus')}/></button>
+                <button id="session-increment" onClick={sessionIncrement}><FontAwesomeIcon icon={solid('circle-plus')}/></button>
             </div>
             <p id="timer-label">{startStop ? "Session" : ""}</p>
             <p id="time-left">{timeLeft}</p>
             <div className="start_stop-controls">
-                <button id="start_stop">{!startStop ? <FontAwesomeIcon icon={solid('play')}/> :
+                <button id="start_stop" onClick={handleStartStop}>{!startStop ? <FontAwesomeIcon icon={solid('play')}/> :
                     <FontAwesomeIcon icon={solid('stop')}/>}</button>
-                <button id="reset"><FontAwesomeIcon icon={solid('backward-fast')}/></button>
+                <button id="reset" onClick={resetClock}><FontAwesomeIcon icon={solid('backward-fast')}/></button>
             </div>
-
         </div>
     );
 }
